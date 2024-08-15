0181b60b5e760e0c7e2e0d164cf365710b45d957
juliomellizo <117930717+juliomellizo@users.noreply.github.com>
2024-08-15 00:47:19 -0500

    Update 2.6
    Ajuste documentacion

--- src/main/java/com/unicauca/maestria/api/gestionegresados/services/curso/CursoServiceImpl.java
+++ src/main/java/com/unicauca/maestria/api/gestionegresados/services/curso/CursoServiceImpl.java
@@ -35,47 +35,48 @@ public class CursoServiceImpl implements CursoService {
     private final ArchivoClientAsignaturas archivoClientAsignaturas;
 
     /**
-     * Obtiene la lista de cursos existentes.
+     * Lista todos los cursos registrados.
      *
-     * @return Una lista de objetos ListadoAsignaturasDto que representan los cursos
-     *         registrados.
+     * @return Lista de ListadoAsignaturasDto que contiene la información de los
+     *         cursos registrados.
      * @throws InformationException si no hay cursos registrados.
      */
     @Override
     @Transactional(readOnly = true)
     public List<ListadoAsignaturasDto> listarCursosExistentes() {
+        // Obtener la lista de cursos desde el cliente de asignaturas
         List<ListadoAsignaturasDto> cursosRegistrados = archivoClientAsignaturas.listarAsignaturas();
+
+        // Verificar si la lista está vacía
         if (cursosRegistrados.isEmpty()) {
             throw new InformationException("No hay cursos registrados");
         }
+
+        // Devolver la lista de cursos
         return cursosRegistrados;
     }
 
     /**
-     * Crea un nuevo curso utilizando la información proporcionada.
+     * Crea un nuevo curso.
      *
-     * @param cursoDto Objeto CursoSaveDto que contiene los datos del curso a crear.
-     * @param result   Objeto BindingResult que contiene los resultados de la
-     *                 validación del formulario.
-     * @return Objeto CursosResponseDto con la información del curso creado.
-     * @throws FieldErrorException si hay errores de validación en el formulario.
+     * @param cursoDto DTO que contiene la información del curso a crear.
+     * @param result   Resultado de la validación del formulario.
+     * @return CursosResponseDto que contiene la información del curso creado.
+     * @throws FieldErrorException si existen errores en el formulario de entrada.
      */
     @Override
     @Transactional
     public CursosResponseDto crear(CursoSaveDto cursoDto, BindingResult result) {
-
+        // Verificar si hay errores de validación en el formulario
         if (result.hasErrors()) {
             throw new FieldErrorException(result);
         }
 
-        // Obtener información del estudiante
-        EstudianteResponseDto informacionEstudiante = archivoClient
-                .obtenerPorIdEstudiante(cursoDto.getIdEstudiante());
-
-        // Obtener información del curso
+        // Obtener la información del estudiante y del curso
+        EstudianteResponseDto informacionEstudiante = archivoClient.obtenerPorIdEstudiante(cursoDto.getIdEstudiante());
         ListadoAsignaturasDto informacionCurso = archivoClientAsignaturas.listarAsignaturaPorId(cursoDto.getIdCurso());
 
-        // Mapear el DTO a la entidad Curso y asignar los valores obtenidos
+        // Mapear el DTO a la entidad Curso y asignar los valores correspondientes
         Curso cursoTmp = cursoMapper.toEntity(cursoDto);
         cursoTmp.setIdEstudiante(informacionEstudiante.getId());
         cursoTmp.setNombre(informacionCurso.getNombreAsignatura());
@@ -89,13 +90,14 @@ public class CursoServiceImpl implements CursoService {
      * Obtiene la información de un curso específico por su ID.
      *
      * @param idCurso ID del curso a buscar.
-     * @return Objeto CursosResponseDto con la información del curso encontrado.
-     * @throws ResourceNotFoundException si no se encuentra el curso con el ID
-     *                                   proporcionado.
+     * @return CursosResponseDto que contiene la información del curso encontrado.
+     * @throws ResourceNotFoundException si el curso con el ID proporcionado no es
+     *                                   encontrado.
      */
     @Override
     @Transactional(readOnly = true)
     public CursosResponseDto obtenerInformacionCurso(Long idCurso) {
+        // Buscar el curso por su ID y convertirlo a un DTO
         return cursoRepository.findById(idCurso)
                 .map(cursoResponseMapper::toDto)
                 .orElseThrow(() -> new ResourceNotFoundException(
@@ -103,19 +105,22 @@ public class CursoServiceImpl implements CursoService {
     }
 
     /**
-     * Lista los cursos dictados por un estudiante específico.
+     * Lista todos los cursos dictados por un estudiante.
      *
-     * @param idEstudiante ID del estudiante cuyos cursos se desean listar.
-     * @return Una lista de objetos CursosResponseDto que representan los cursos
-     *         dictados por el estudiante.
+     * @param idEstudiante ID del estudiante cuyos cursos se listarán.
+     * @return Una lista de CursosResponseDto con la información de los cursos
+     *         dictados.
      */
     @Override
     @Transactional(readOnly = true)
     public List<CursosResponseDto> listarCursosDictados(Long idEstudiante) {
+        // Verificar la existencia del estudiante
         archivoClient.obtenerPorIdEstudiante(idEstudiante);
 
+        // Buscar los cursos asociados al estudiante en la base de datos
         List<Curso> cursos = cursoRepository.findByEstudianteId(idEstudiante);
 
+        // Convertir las entidades encontradas en una lista de DTOs de respuesta
         return cursos.stream()
                 .map(cursoResponseMapper::toDto)
                 .collect(Collectors.toList());
@@ -125,31 +130,34 @@ public class CursoServiceImpl implements CursoService {
      * Actualiza la información de un curso existente.
      *
      * @param id       ID del curso a actualizar.
-     * @param cursoDto Objeto CursoSaveDto que contiene los datos del curso a
-     *                 actualizar.
-     * @param result   Objeto BindingResult que contiene los resultados de la
-     *                 validación del formulario.
-     * @return Objeto CursosResponseDto con la información del curso actualizado.
-     * @throws FieldErrorException       si hay errores de validación en el
-     *                                   formulario.
-     * @throws ResourceNotFoundException si no se encuentra el curso con el ID
-     *                                   proporcionado.
+     * @param cursoDto DTO que contiene la información actualizada del curso.
+     * @param result   Resultado de la validación del formulario.
+     * @return CursosResponseDto que contiene la información del curso actualizado.
+     * @throws FieldErrorException       si existen errores en el formulario de
+     *                                   entrada.
+     * @throws ResourceNotFoundException si el curso con el ID proporcionado no es
+     *                                   encontrado.
      */
     @Override
     public CursosResponseDto actualizar(Long id, CursoSaveDto cursoDto, BindingResult result) {
-
+        // Verificar si hay errores de validación en el formulario
         if (result.hasErrors()) {
             throw new FieldErrorException(result);
         }
 
+        // Verificar la existencia del estudiante asociado al curso
         archivoClient.obtenerPorIdEstudiante(cursoDto.getIdEstudiante());
 
+        // Buscar el curso en la base de datos por su ID
         Curso cursoTmp = cursoRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("Curso con id " + id + " no encontrado"));
 
+        // Actualizar la información del curso con los nuevos datos del DTO
         updateCurso(cursoTmp, cursoDto);
-        Curso responseCurso = cursoRepository.save(cursoTmp);
 
+        // Guardar el curso actualizado en la base de datos y devolver la respuesta
+        // mapeada
+        Curso responseCurso = cursoRepository.save(cursoTmp);
         return cursoResponseMapper.toDto(responseCurso);
     }
 
@@ -157,29 +165,33 @@ public class CursoServiceImpl implements CursoService {
      * Elimina un curso por su ID.
      *
      * @param id ID del curso a eliminar.
-     * @throws ResourceNotFoundException si no se encuentra el curso con el ID
-     *                                   proporcionado.
+     * @throws ResourceNotFoundException si el curso con el ID proporcionado no es
+     *                                   encontrado.
      */
     @Override
     @Transactional
     public void eliminar(Long id) {
+        // Buscar el curso en la base de datos por su ID
         cursoRepository.findById(id)
                 .orElseThrow(
                         () -> new ResourceNotFoundException(
                                 "Curso con id: " + id + " no encontrado"));
+
+        // Eliminar el curso de la base de datos
         cursoRepository.deleteById(id);
     }
 
     /**
-     * Actualiza los detalles de un curso existente.
+     * Actualiza los detalles de una entidad Curso.
      *
-     * @param curso    Entidad Curso que se desea actualizar.
-     * @param cursoDto Objeto CursoSaveDto que contiene los nuevos datos del curso.
+     * @param curso    La entidad Curso a actualizar.
+     * @param cursoDto DTO que contiene los nuevos valores para la entidad Curso.
      */
     private void updateCurso(Curso curso, CursoSaveDto cursoDto) {
-
+        // Obtener la información del curso desde el cliente de asignaturas
         ListadoAsignaturasDto informacionCurso = archivoClientAsignaturas.listarAsignaturaPorId(cursoDto.getIdCurso());
 
+        // Asignar los nuevos valores a la entidad Curso
         curso.setNombre(informacionCurso.getNombreAsignatura());
         curso.setOrientadoA(cursoDto.getOrientadoA());
         curso.setFechaInicio(cursoDto.getFechaInicio());
--- src/main/java/com/unicauca/maestria/api/gestionegresados/services/informacion_general/InformacionGeneralServiceImpl.java
+++ src/main/java/com/unicauca/maestria/api/gestionegresados/services/informacion_general/InformacionGeneralServiceImpl.java
@@ -12,6 +12,7 @@ import com.unicauca.maestria.api.gestionegresados.dtos.EstudianteResponseDto;
 import com.unicauca.maestria.api.gestionegresados.dtos.InformacionEstudiantesResponseDto;
 import com.unicauca.maestria.api.gestionegresados.dtos.InformacionGeneralResponseDto;
 import com.unicauca.maestria.api.gestionegresados.exceptions.InformationException;
+import com.unicauca.maestria.api.gestionegresados.exceptions.ResourceNotFoundException;
 
 @Service
 @RequiredArgsConstructor
@@ -19,15 +20,27 @@ public class InformacionGeneralServiceImpl implements InformacionGeneralService 
 
     private final ArchivoClient archivoClient;
 
+    /**
+     * Obtiene una lista de estudiantes registrados y mapea la información a un DTO
+     * reducido.
+     * 
+     * @return Una lista de InformacionEstudiantesResponseDto que contiene la
+     *         información básica de los estudiantes.
+     * @throws InformationException si no hay estudiantes registrados.
+     */
     @Override
     @Transactional(readOnly = true)
     public List<InformacionEstudiantesResponseDto> obtenerEstudiantes() {
+        // Obtener la información de los estudiantes desde un cliente externo
         List<EstudianteResponseDto> informacionEstudiantes = archivoClient.obtenerEstudiantes();
 
-        if(informacionEstudiantes.isEmpty()){
+        // Verifica si la lista de estudiantes está vacía y lanza una excepción si es el
+        // caso
+        if (informacionEstudiantes.isEmpty()) {
             throw new InformationException("No hay estudiantes registrados");
         }
 
+        // Mapea la información de los estudiantes a un DTO reducido
         List<InformacionEstudiantesResponseDto> estudiantesReducidos = informacionEstudiantes.stream()
                 .map(estudiante -> new InformacionEstudiantesResponseDto(
                         estudiante.getId(),
@@ -36,13 +49,26 @@ public class InformacionGeneralServiceImpl implements InformacionGeneralService 
                         estudiante.getPersona().getNombre(),
                         estudiante.getPersona().getApellido()))
                 .collect(Collectors.toList());
+
         return estudiantesReducidos;
     }
 
+    /**
+     * Busca la información completa de un estudiante por su ID.
+     * 
+     * @param idEstudiante El ID del estudiante a buscar.
+     * @return Un InformacionGeneralResponseDto con la información completa del
+     *         estudiante.
+     * @throws ResourceNotFoundException si el estudiante no es encontrado.
+     */
     @Override
     @Transactional(readOnly = true)
     public InformacionGeneralResponseDto buscarEstudiante(Long idEstudiante) {
+        // Obtiene la información detallada de un estudiante por su ID desde un cliente
+        // externo
         EstudianteResponseDto informacionEstudiantes = archivoClient.obtenerPorIdEstudiante(idEstudiante);
+
+        // Mapea la información obtenida a un DTO para su uso en la capa de respuesta
         InformacionGeneralResponseDto informacionGeneralResponseDto = InformacionGeneralResponseDto.builder()
                 .id(informacionEstudiantes.getId())
                 .codigo(informacionEstudiantes.getCodigo())
@@ -58,4 +84,5 @@ public class InformacionGeneralServiceImpl implements InformacionGeneralService 
 
         return informacionGeneralResponseDto;
     }
+
 }
